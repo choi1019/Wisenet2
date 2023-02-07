461c5a8e93881b11d01238788c3ea89100601d7b
choi1019 <choi.sungwoon@gmail.com>
2023-02-07 17:19:25 +0900

    v.0.0

---
+++ 02Platform/typedef.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <typedef.h>
+
+enum class _ELayer_Platform {
+	_eBegin = _GET_LAYER_UID(_ELayer::_ePlatform),
+
+	_eUId,
+	_eEvent,
+	_eEventQueue,
+	_eComponentPart,
+	_eComponent,
+	_eComponentStub,
+	_eScheduler,
+	_eLifecycleManager,
+	_eMain,
+
+	_eEnd
+};
+
+#define MAXCOMPONENTPARTS 20
+#define MAXRECEIVERCOMPONENTS 20
+#define MAXTARGETCOMPONENTS 10
+#define MAXTARGETGROUPS 10
