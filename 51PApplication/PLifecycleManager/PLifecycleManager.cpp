bce4d5dc6d1424e63f7bd0394b8ec7fd823d9aeb
choi1019 <choi.sungwoon@gmail.com>
2023-02-14 13:19:03 +0900

    1

--- 51PApplication/PLifecycleManager/PLifecycleManager.cpp
+++ 51PApplication/PLifecycleManager/PLifecycleManager.cpp
@@ -14,17 +14,17 @@ PLifecycleManager::~PLifecycleManager() {}
 
 void PLifecycleManager::RegisterUserShedulers() {
 	this->RegisterAScheduler((int)EComponents::eScheduler1, new("eScheduler1") PScheduler());
-	this->RegisterAScheduler((int)EComponents::eScheduler2, new("eScheduler2") PScheduler());
+//	this->RegisterAScheduler((int)EComponents::eScheduler2, new("eScheduler2") PScheduler());
 	// this->RegisterAScheduler((int)EComponents::eVideoInput, new("eVideoInput") VideoInput());
 }
 void PLifecycleManager::RegisterUserComponents() {
 	this->RegisterAComponent((int)EComponents::eTimer1, new("eTimerLinux1") PTimerLinux(500));
-	this->RegisterAComponent((int)EComponents::eTimer2, new("eTimerLinux2") PTimerLinux(500));
+//	this->RegisterAComponent((int)EComponents::eTimer2, new("eTimerLinux2") PTimerLinux(500));
 	// this->RegisterAComponent((int)EComponents::eVideoManager, new("VideoManager") VideoManager());
 }
 void PLifecycleManager::AllocateUserComponents() {
 	this->AllocateAComponent((int)EComponents::eTimer1, (int)EComponents::eScheduler1);
-	this->AllocateAComponent((int)EComponents::eTimer2, (int)EComponents::eScheduler2);
+//	this->AllocateAComponent((int)EComponents::eTimer2, (int)EComponents::eScheduler2);
 	// this->AllocateAComponent((int)EComponents::eVideoManager, (int)EComponents::eScheduler1);
 }
 void PLifecycleManager::AssociateUserSendersNReceivers() {
@@ -49,7 +49,7 @@ void PLifecycleManager::StartComponents() {
 	BaseObject::s_pMemory->Show("Static");
 	ValueObject::s_pMemory->Show("Dynamic");
 	this->m_mapComponents[(int)EComponents::eTimer1]->Start();
-	this->m_mapComponents[(int)EComponents::eTimer2]->Start();;
+//	this->m_mapComponents[(int)EComponents::eTimer2]->Start();;
 }
 
 void PLifecycleManager::StopComponents() {
